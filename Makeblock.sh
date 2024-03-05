c2b5c94b7e66867fcbe69bebba4893eaea7cef44
tomatito <german.m.garcia@gmail.com>
2024-03-05 17:18:46 +0100

    launch files updated

--- Makeblock.sh
+++ Makeblock.sh
@@ -3,14 +3,12 @@
 source ~/.bashrc
 
 gnome-terminal  -x $SHELL -ic "roscore"
-sleep 5
+sleep 4
 gnome-terminal  -x $SHELL -ic  'mon launch robot_launch makeblock.launch'
 
-gnome-terminal  -x $SHELL -ic "rosrun mk_odometry odometry.py"
-
 sleep 1;
-gnome-terminal  -x $SHELL -ic "source /home/tomatito/ws/devel_isolated/setup.bash; cd ~/ws/src/cartographer_ros/cartographer_ros/launch; mon launch makeblock.launch";
-
+#gnome-terminal  -x $SHELL -ic "source /home/tomatito/ws/devel_isolated/setup.bash; cd ~/ws/src/cartographer_ros/cartographer_ros/launch; mon launch makeblock.launch";
+gnome-terminal -x $SHELL -ic 'mon launch robot_launch cartographer.launch'
 
 
 exit 0
